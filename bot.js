1766c85860fced80b1a87d55b0c8db6a0010be52
vcinonyx <vcinonyx@gmail.com>
2020-12-04 00:34:01 +0200

    !clear func

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on("message", msg => {
     modUser(msg.member)
   }
 
-  if(msg.content === `${BOT_PREFIX}${CLEAR_COMMAND}`) {
+  if(msg.content.includes(`${BOT_PREFIX}${CLEAR_COMMAND}`)) {
     clear(msg);
   }
 
@@ -31,27 +31,27 @@ function modUser(member) {
   member.roles.add("783084095223234590")
 }
 
-// function clear(mess) { // Создание новой функции с командой
+function clear(mess) { // Создание новой функции с командой
 
-//   const arggs = mess.content.split(' ').slice(1);
+  const arggs = mess.content.split(' ').slice(1);
  
-//   const amount = arggs.join(' ');
+  const amount = arggs.join(' ');
   
-//   if (!amount) return mess.channel.send('Вы не указали, сколько сообщений нужно удалить!'); 
+  if (!amount) return mess.channel.send('Вы не указали, сколько сообщений нужно удалить!'); 
  
-//   if (isNaN(amount)) return mess.channel.send('Это не число!'); 
+  if (isNaN(amount)) return mess.channel.send('Это не число!'); 
  
-//    if (amount > 100) return mess.channel.send('Вы не можете удалить 100 сообщений за раз');
-//    if (amount < 1) return mess.channel.send('Вы должны ввести число больше чем 1');
+   if (amount > 100) return mess.channel.send('Вы не можете удалить 100 сообщений за раз');
+   if (amount < 1) return mess.channel.send('Вы должны ввести число больше чем 1');
          
-//          async function delete_messages() {
+         async function delete_messages() {
  
-//          await mess.channel.messages.fetch({ limit: amount }).then(messages => {
-//              mess.channel.bulkDelete(messages)
-//              mess.channel.send(`Удалено ${amount} сообщений!`)
-//          })};
-//          delete_messages(); 
-//  }
+         await mess.channel.messages.fetch({ limit: amount }).then(messages => {
+             mess.channel.bulkDelete(messages)
+             mess.channel.send(`Удалено ${amount} сообщений!`)
+         })};
+         delete_messages(); 
+ }
 
 client.login(process.env.BOT_TOKEN)
 
