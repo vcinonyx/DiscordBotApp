cef4f435258bf7fa2e5a6307bf1c126f96aa3abc
vcinonyx <vcinonyx@gmail.com>
2020-12-04 15:47:28 +0200

    heads or tails command added

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const client = new Discord.Client({
 const BOT_PREFIX = "!"
 const MOD_ME_COMMAND = "mod-me"
 const CLEAR_COMMAND = "clear"
-
+const HEADS_OR_TAILS_COMMAND ="heads"
 
 client.on("message", msg => {
       if (msg.content == "I love teorver") {
@@ -22,34 +22,50 @@ client.on("message", msg => {
     clear(msg);
   }
 
+  if(msg.content === `${BOT_PREFIX}${HEADS_OR_TAILS_COMMAND}`){
+    heads_or_tails(msg);
+  }
 })
 
+
+function heads_or_tails(mess) {
+  mess.channel.send('Монета подбрасывается...')
+
+  var random = Math.floor(Math.random() * 4) + 1; // Объявление переменной random - она вычисляет случайное число от 1 до 3
+
+  if (random === 1) { // Если вычислено число 1, то выпадает орёл.
+    mess.channel.send(':full_moon: Орёл!')
+  } else if (random === 2) { // Если вычислено число 2, то выпадает решка.
+    mess.channel.send(':new_moon: Решка!') 
+  } else if (random === 3) { // Если вычислено число 3, то монета падает ребром.
+    mess.channel.send(':last_quarter_moon: Монета упала ребром!')
+}
+}
 
 
 function modUser(member) {
   member.roles.add("783084095223234590")
 }
 
-function clear(mess) { // Создание новой функции с командой
 
+
+
+function clear(mess) { // Создание новой функции с командой
   const arggs = mess.content.split(' ').slice(1);
- 
   const amount = arggs.join(' ');
   
   if (!amount) return mess.channel.send('Вы не указали, сколько сообщений нужно удалить!'); 
- 
   if (isNaN(amount)) return mess.channel.send('Это не число!'); 
  
-   if (amount > 100) return mess.channel.send('Вы не можете удалить 100 сообщений за раз');
-   if (amount < 1) return mess.channel.send('Вы должны ввести число больше чем 1');
-         
-         async function delete_messages() {
- 
-         await mess.channel.messages.fetch({ limit: amount }).then(messages => {
-             mess.channel.bulkDelete(messages)
-             mess.channel.send(`Удалено ${amount} сообщений!`)
-         })};
-         delete_messages(); 
+  if (amount > 100) return mess.channel.send('Вы не можете удалить 100 сообщений за раз');
+  if (amount < 1) return mess.channel.send('Вы должны ввести число больше чем 1');
+  
+    async function delete_messages() {
+      await mess.channel.messages.fetch({ limit: amount }).then(messages => {
+          mess.channel.bulkDelete(messages)
+          mess.channel.send(`Удалено ${amount} сообщений!`)
+      })};  
+      delete_messages(); 
  }
 
  client.login(process.env.token)
